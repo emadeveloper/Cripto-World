11d6f562285a199c70e14fae2d0416e6137cd13d
Emanuel Martinez <yinproducciones@gmail.com>
2023-11-22 15:03:46 -0300

    starting the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import {Switch, Route, Link} from 'react-router-dom'
-import {Layout, Typography, Space, Divider} from 'antd';
-import {Navbar} from './components'
-import './App.css'
-
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News
+} from "./components";
+import "./App.css";
+import { AlignCenterOutlined } from "@ant-design/icons";
 
 const App = () => {
   return (
-    <div className='app'>
-        <div className="navbar">
-            <Navbar />
-        </div>
-        <div className="main">
-
-        </div>
-        <div className="footer">
-
-        </div>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+           <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+           </Routes>
+          </div>
+        </Layout>
+      <div className="footer">
+          <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+            Cripto World <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
